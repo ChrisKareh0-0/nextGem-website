fdec2daf6151acb6b7405dbfb2779614f159022b
Chris <chriskareh@proton.me>
2025-04-16 09:56:36 +0300

    fixed cuber animation size

--- src/Components/CubeAnimation.jsx
+++ src/Components/CubeAnimation.jsx
@@ -355,8 +355,8 @@ const CubeAnimation = () => {
     <div 
       ref={containerRef} 
       style={{ 
-        width: '100%', 
-        height: '100%',
+        width: '50%', 
+        height: '50%',
         position: 'relative',
         overflow: 'hidden',
         background: 'transparent'
